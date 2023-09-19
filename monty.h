73d67284aa1be1b1507a717480c27bbf6f37d3bc
Rosemary Okonda <olisaokonda@gmail.com>
2023-09-19 15:33:24 +0000

    .h file for monty project

--- monty.h
+++ monty.h
@@ -11,6 +11,11 @@
 #include <ctype.h>
 #include <string.h>
 
+#define SIZE 1024
+#define EXIT exit(EXIT_FAILURE)
+
+extern int stack_value;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -41,5 +46,52 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct opcode - opcode identity plus value
+ * @opcode_identity: identity of the opcode function
+ * @opcode_value: value of opcode given
+ *
+ * Description: Ability to store opcode identity and value in
+ * stack
+ */
+typedef struct opcode
+{
+	char *opcode_identity;
+	int opcode_value;
+} opcode_t;
+
+/* function declarations for Monty project */
+
+void Errormngt(int, char *, int);
+char **_strtokenize(char *);
+
+int Strcount(char *, char *);
+
+opcode_t *Strtokenizationcommand(char *, unsigned int);
+
+void Opc_push(stack_t **stack, unsigned int line_number);
+void Opc_pall(stack_t **stack, unsigned int line_number);
+void Opc_pop(stack_t **stack, unsigned int line_number);
+void Opc_pint(stack_t **stack, unsigned int line_number);
+void Opc_swap(stack_t **stack, unsigned int line_number);
+void Opc_add(stack_t **stack, unsigned int line_number);
+void Opc_nop(stack_t **stack, unsigned int line_number);
+void Opc_sub(stack_t **stack, unsigned int line_number);
+void Opc_div(stack_t **stack, unsigned int line_number);
+void Opc_mul(stack_t **stack, unsigned int line_number);
+void Opc_mod(stack_t **stack, unsigned int line_number);
+void Opc_pchar(stack_t **stack, unsigned int line_number);
+void Opc_pstr(stack_t **stack, unsigned int line_number);
+void Opc_rotl(stack_t **stack, unsigned int line_number);
+void Opc_rotr(stack_t **stack, unsigned int line_number);
+
+void free_stack(stack_t **);
+void Handle_stack(stack_t **stack, unsigned int line_number);
+void Handle_queue(stack_t **stack, unsigned int line_number);
+
+int rotr_rec(stack_t *, stack_t **);
+int _isascii(int);
+int CountStack(stack_t **);
 
 #endif
